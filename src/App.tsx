15f4f988224ebb74218544e2865af184628952fb
dalton barnett <daltonbarnett21@yahoo.com>
2023-04-11 21:47:34 -0500

    added logic for multiselect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+const companies = [
+  { companyName: "Bobs boats" },
+  { companyName: "Johns Hardware" },
+];
+
 function App() {
+  const [isCheckAll, setIsCheckAll] = useState(false);
+  const [isCheck, setIsCheck] = useState<any>([]);
+  const [list, setList] = useState<any>([]);
+
+  useEffect(() => {
+    setList(companies);
+  }, [list]);
+
+  const handleSelectAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsCheckAll(!isCheckAll);
+    setIsCheck(list.map((li: any) => li.companyName));
+    if (isCheckAll) {
+      setIsCheck([]);
+    }
+  };
+
+  const handleClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    setIsCheck([...isCheck, name]);
+    if (!checked) {
+      setIsCheck(isCheck.filter((item: any) => item !== name));
+    }
+  };
+
   return (
     <>
-      <h1>Hello</h1>
+      <ul>
+        <li>
+          <input
+            type="checkbox"
+            onChange={handleSelectAll}
+            checked={isCheckAll}
+            name="selectAll"
+            id="selectAll"
+          />
+          <label>All</label>
+        </li>
+        {list?.map(({ companyName }: any) => (
+          <li key={companyName}>
+            <input
+              type="checkbox"
+              name={companyName}
+              id={companyName}
+              onChange={handleClick}
+              checked={isCheck.includes(companyName)}
+            />
+            <label>{companyName}</label>
+          </li>
+        ))}
+      </ul>
+      <div>
+        {isCheck.length === companies.length ? <span>All</span> : isCheck}
+      </div>
     </>
   );
 }
